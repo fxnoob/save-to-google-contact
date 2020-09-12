737fde09fc86539a6eba3db5e3782b3ea7b6cfc0
fxnoob <fxnoob71@gmail.com>
2020-09-12 17:08:09 +0530

    refine build modes II

--- webpack.config.js
+++ webpack.config.js
@@ -2,73 +2,71 @@ const webpack = require("webpack");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const { manifestTransform } = require("./scripts/transform");
 
-module.exports = {
-  entry: {
-    background: "./src/background.js",
-    option: "./src/option-page/App.jsx",
-    popup: "./src/popup-page/App.jsx",
-    content_script: "./src/content-scripts/App.jsx"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"]
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.iframe.css$/,
-        use: [{ loader: "file-loader" }, { loader: "postcss-loader" }]
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-          "file-loader",
-          {
-            loader: "image-webpack-loader",
-            options: {
-              bypassOnDebug: true, // webpack@1.x
-              disable: true // webpack@2.x and newer
+module.exports = (env, options) => {
+  return {
+    entry: {
+      content_script: "./src/content-scripts/App.jsx",
+      background: "./src/background.js",
+      popup: "./src/popup-page/App.jsx",
+      option: "./src/option-page/App.jsx"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ["babel-loader"]
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"]
+        },
+        {
+          test: /\.(gif|png|jpe?g|svg)$/i,
+          use: [
+            "file-loader",
+            {
+              loader: "image-webpack-loader",
+              options: {
+                bypassOnDebug: true, // webpack@1.x
+                disable: true // webpack@2.x and newer
+              }
             }
-          }
-        ]
-      }
-    ]
-  },
-  resolve: {
-    extensions: ["*", ".js", ".jsx", ".css", ".json"]
-  },
-  output: {
-    path: __dirname + "/dist",
-    publicPath: "/",
-    filename: "[name].bundle.js"
-  },
-  plugins: [
-    new CopyWebpackPlugin(
-      [
-        { from: "./src/option-page/option.html", force: true },
-        { from: "./src/popup-page/popup.html", force: true },
-        { from: "./src/app/", force: true }
-      ],
-      {}
-    ),
-    new CopyWebpackPlugin([
-      {
-        from: "./src/app/manifest.json",
-        force: true,
-        transform(content, path) {
-          return manifestTransform(content, path, options);
+          ]
         }
-      }
-    ]),
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  devServer: {
-    contentBase: "./dist",
-    hot: true
-  }
+      ]
+    },
+    resolve: {
+      extensions: ["*", ".js", ".jsx", ".json", ".css"]
+    },
+    output: {
+      path: __dirname + "/dist",
+      publicPath: "/",
+      filename: "[name].bundle.js"
+    },
+    plugins: [
+      new CopyWebpackPlugin(
+        [
+          { from: "./src/popup-page/popup.html", force: true },
+          { from: "./src/option-page/option.html", force: true },
+          { from: "./src/app/", force: true }
+        ],
+        {}
+      ),
+      new CopyWebpackPlugin([
+        {
+          from: "./src/app/manifest.json",
+          force: true,
+          transform(content, path) {
+            return manifestTransform(content, path, options);
+          }
+        }
+      ]),
+      new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+      contentBase: "./dist",
+      hot: true
+    }
+  };
 };
